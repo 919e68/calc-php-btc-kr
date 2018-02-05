4bf4bebc313f3c9658d3b11afc562f79225c78db
wilson <konekred@gmail.com>
2018-02-05 20:49:50 +0800

    up

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 const Cycler = require('./lib/Cycler')
+const Converter = require('./lib/Converter')
 
-Cycler.convert(40000000).then(finalPHP => {
-  console.log(finalPHP)
+
+Cycler.convertCoinsPHToBithumb(400000).then(data => {
+  console.log('CoinsPH To Bithumb', JSON.stringify(data, null, 2))
+})
+
+Cycler.convertBithumbToCoinsPH(400000).then(data => {
+  console.log('Bithumb To CoinsPH', JSON.stringify(data, null, 2))
+})
+
+Converter.convert(1, 'PHP', 'KRW').then(price => {
+  console.log('1 PHP = ' + price + ' KRW')
 })
